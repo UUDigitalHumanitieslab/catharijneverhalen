3ccb49d40219725bb3c5b95cb78357f0dfdae123
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-08-17 11:48:12 +0200

    Update all of the JavaScript dependencies

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -4,6 +4,7 @@ module.exports = function(config){
 		basePath : 'app/',
 
 		files : [
+			'bower_components/lodash/lodash.js',
 			'bower_components/angular/angular.js',
 			'bower_components/angular-route/angular-route.js',
 			'bower_components/angular-mocks/angular-mocks.js',
