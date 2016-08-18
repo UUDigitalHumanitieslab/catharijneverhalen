0f1a769860bcbb33c5335db2c4fda80fa0d7c40b
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-08-18 11:23:31 +0200

    Test the route config on catharijne.toevoegen as well

--- frontend/app/views/toevoegen/toevoegen_test.js
+++ frontend/app/views/toevoegen/toevoegen_test.js
@@ -1,8 +1,25 @@
 'use strict';
 
 describe('catharijne.toevoegen', function() {
+	var $routeProvider;
+	
 	beforeEach(function() {
+		angular.module('testAssist', [
+			'ngRoute'
+		]).config(function(_$routeProvider_) {
+			$routeProvider = _$routeProvider_;
+			spyOn($routeProvider, 'when');
+		});
+		module('testAssist');
 		module('catharijne.toevoegen');
+	});
+	
+	it('routes on /toevoegen with toevoegen.html and StoryFormCtrl', function() {
+		inject();
+		expect($routeProvider.when).toHaveBeenCalledWith('/toevoegen', {
+			templateUrl: 'views/toevoegen/toevoegen.html',
+			controller: 'StoryFormCtrl'
+		});
 	});
 	
 	describe('StoryFormCtrl', function() {
