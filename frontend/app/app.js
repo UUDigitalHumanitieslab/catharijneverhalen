d271c5c6fb81c6ad9c4024707a97f79f3e79177a
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-08-23 12:55:12 +0200

    Reorder dependencies of app module to reflect HTML order

--- frontend/app/app.js
+++ frontend/app/app.js
@@ -3,19 +3,18 @@
 // Declare app level module which depends on views, and components
 angular.module('catharijne', [
 	'ngRoute',
-	'catharijne.version',
 	'catharijne.menu',
 	'catharijne.block',
 	'catharijne.blocklist',
+	'catharijne.version',
+	'catharijne.googleMaps',
 	'catharijne.home',
 	'catharijne.meedoen',
 	'catharijne.aanmelden',
 	'catharijne.inloggen',
 	'catharijne.verhaalofprofiel',
 	'catharijne.profiel',
-	'catharijne.toevoegen',
-	'catharijne.googleMaps',
-	'catharijne.googleMaps.apiKey'
+	'catharijne.toevoegen'
 ]).
 config(['$routeProvider', function($routeProvider) {
 	$routeProvider.otherwise({redirectTo: '/home'});
