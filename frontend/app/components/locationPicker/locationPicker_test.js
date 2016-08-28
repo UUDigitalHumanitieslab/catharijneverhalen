cde9b48b16b91389b6f0db5f5c408731c06b7fcd
Julian Gonggrijp <j.gonggrijp@gmail.com>
2016-08-28 13:06:55 +0200

    Factor out the button/widget/childScope logic

--- frontend/app/components/locationPicker/locationPicker_test.js
+++ frontend/app/components/locationPicker/locationPicker_test.js
@@ -20,7 +20,7 @@ describe('catharijne.locationPicker module', function() {
 	});
 	
 	describe('appLocationPicker directive', function() {
-		var scope, elementFunc, element;
+		var scope, elementFunc, element, button, widget, childScope;
 		var getChild = function(query) {
 			var rawChild;
 			if (query) {
@@ -45,24 +45,23 @@ describe('catharijne.locationPicker module', function() {
 			beforeEach(function() {
 				element = elementFunc(scope);
 				scope.$digest();
+				button = getChild('button');
+				childScope = button.scope();
 			});
 			
 			it('uses the Netherlands as a backup location', function() {
 				inject(function(locationDefaults) {
 					expect(scope.map).toEqual(locationDefaults);
-					var childScope = getChild().scope();
 					expect(childScope.location).toEqual(locationDefaults);
 				});
 			});
 			
 			it('shows a button for choosing a location', function() {
-				var button = element.find('button');
 				expect(button.length).toBe(1);
 				expect(button.text()).toBe('Kies een locatie');
 			});
 			
 			it('opens a larger map when clicked', function() {
-				var button = getChild('button');
 				button.triggerHandler('click');
 				expect(getChild('button').length).toBe(0);
 				var map = getChild('ui-gmap-google-map');
@@ -71,13 +70,11 @@ describe('catharijne.locationPicker module', function() {
 			});
 			
 			it('does not display a marker initially', function() {
-				var button = getChild('button');
-				var subScope = button.scope();
 				button.triggerHandler('click');
 				inject(function(uiGmapGoogleMapApi) {
 					uiGmapGoogleMapApi.then(function(gMapsApi) {
-						expect(subScope.handle.getGMap).toBeDefined();
-						expect(subScope.handle.getGMarkers).not.toBeDefined();
+						expect(childScope.handle.getGMap).toBeDefined();
+						expect(childScope.handle.getGMarkers).not.toBeDefined();
 					});
 				});
 			});
@@ -95,28 +92,27 @@ describe('catharijne.locationPicker module', function() {
 				scope.map = new Object(priorLocation);
 				element = elementFunc(scope);
 				scope.$digest();
+				widget = getChild('.app-location');
+				childScope = widget.scope();
 			});
 			
 			it('uses the prior location', function() {
-				expect(getChild().scope().location).toEqual(priorLocation);
+				expect(childScope.location).toEqual(priorLocation);
 				expect(scope.map).toEqual(priorLocation);
 			});
 			
 			it('shows the location', function() {
-				var widget = getChild('.app-location');
 				expect(widget.length).toBe(1);
 				var subWidget = angular.element(widget.children()[0]);
 				expect(subWidget.scope().properties).toEqual(priorLocation);
 			});
 			
 			it('does display a marker initially', function() {
-				var button = getChild('button');
-				var subScope = button.scope();
 				button.triggerHandler('click');
 				inject(function(uiGmapGoogleMapApi) {
 					uiGmapGoogleMapApi.then(function(gMapsApi) {
-						expect(subScope.handle.getGMarkers).toBeDefined();
-						expect(subScope.handle.getGMarkers().length).toBe(1);
+						expect(childScope.handle.getGMarkers).toBeDefined();
+						expect(childScope.handle.getGMarkers().length).toBe(1);
 					});
 				});
 			});
