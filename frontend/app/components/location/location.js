40d5c1e5b334b4d3610447ca91f3f81772c140b4
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-08-29 16:24:01 +0200

    Fine-tune the default location

--- frontend/app/components/location/location.js
+++ frontend/app/components/location/location.js
@@ -3,8 +3,8 @@
 angular.module('catharijne.location', [
 	'catharijne.googleMaps'
 ]).constant('locationDefaults', {  // The Netherlands
-	coords: {latitude: 52.4, longitude: 4.5},
-	zoom: 9
+	coords: {latitude: 52.19, longitude: 5.31},
+	zoom: 7
 }).directive('appLocation', function() {return {
 	templateUrl: 'components/location/location.html',
 	controller: 'LocationCtrl',
