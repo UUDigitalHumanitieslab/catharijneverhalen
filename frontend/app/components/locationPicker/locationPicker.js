ac8b96c5aa88e98f8c875fdbf877c4945d9977f9
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-08-31 18:19:27 +0200

    Get stuck in endless debugging

--- frontend/app/components/locationPicker/locationPicker.js
+++ frontend/app/components/locationPicker/locationPicker.js
@@ -13,7 +13,9 @@ angular.module('catharijne.locationPicker', [
 }).controller('LocationPickerCtrl', [
 	'locationDefaults',
 	'$scope',
-	function(locationDefaults, $scope) {
+	'uiGmapGoogleMapApi',
+	'uiGmapIsReady',
+	function(locationDefaults, $scope, uiGmapGoogleMapApi, uiGmapIsReady) {
 		$scope.handle = {};
 		$scope.active = false;
 		$scope.activate = function() {
@@ -21,6 +23,7 @@ angular.module('catharijne.locationPicker', [
 			$scope.kickedOff = true;
 		};
 		$scope.deactivate = function(event) {
+			$scope.debugEvent(event);
 			$scope.active = false;
 		};
 		$scope.zoom = $scope.location.zoom || locationDefaults.zoom;
@@ -28,19 +31,39 @@ angular.module('catharijne.locationPicker', [
 			{},
 			$scope.location.coords || locationDefaults.coords
 		);
-		$scope.has_picked = ($scope.location.id != undefined);
+		var expectedMapInstances = 1;
+		if ($scope.has_picked) expectedMapInstances++;
+		// uiGmapGoogleMapApi.then(function(api) {
+		// 	uiGmapIsReady.promise(expectedMapInstances).then(function(maps) {
+		// 		var map = $scope.handle.getGMap();
+		// 		api.event.addDomListener(map, 'click', function(event) {
+		// 			event.stop();
+		// 		});
+		// 	});
+		// });
+		$scope.debugEvent = function(event) {
+			console.debug(event.constructor.toString(), 'on', event.target);
+		};
 		$scope.mapEvents = {
 			click: function(map, eventName, args) {
+				// args[0].stop();
 				var newCoords = args[0].latLng;
 				$scope.location.coords = {
 					latitude: newCoords.lat(),
 					longitude: newCoords.lng()
 				};
+				console.debug($scope.location.coords);
 				$scope.location.zoom = map.getZoom();
 				if (! $scope.location.id) $scope.location.id = 'temp';
-				$scope.has_picked = true;
-				$scope.$digest();
+				// $scope.$digest();
 			}
+		};
+		this.$doCheck = function() {
+			console.debug({
+				active: $scope.active,
+				center: $scope.center,
+				hasPicked: $scope.location.id
+			});
 		};
 	}
 ]);
