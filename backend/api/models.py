d0ead8290f6a6a3b38c9a32f5d18dfe0c6b5719b
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-09-28 17:37:48 +0200

    Define Person model with helpers

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,72 @@
 from django.db import models
+from django.core import validators
 
-# Create your models here.
+
+class Category(models.Model):
+    """ Abstract base for models that represent category levels. """
+    name = models.CharField(max_length=30)
+    
+    class Meta():
+        abstract = True
+
+
+class Person(models.Model):
+    """ Person description similar to the approach in MCC AdLib. """
+    GENDER_CHOICES = (
+        (1, 'female'),
+        (2, 'male'),
+        (3, 'other'),
+    )
+    name = models.CharField(blank=True, max_length=254)
+    address_place = models.CharField(blank=True, max_length=126)
+    birth_year = models.SmallIntegerField(null=True, blank=True, validators=[
+        validators.MinValueValidator(-2000),
+        validators.MaxValueValidator(2017),
+    ])
+    birth_place = models.CharField(blank=True, max_length=126)
+    gender = models.SmallIntegerField(
+        null=True, 
+        blank=True,
+        choices=GENDER_CHOICES,
+    )
+    family_composition = models.TextField(blank=True)
+    education_level = models.ForeignKey(
+        'EducationLevel',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    occupation = models.CharField(blank=True, max_length=126)
+    marital_status = models.ForeignKey(
+        'MaritalStatus',
+        on_delete=models.SET_NULL,
+        null=True,
+        blank=True,
+    )
+    religious_background = models.CharField(blank=True, max_length=126)
+
+
+class EducationLevel(Category):
+    """ Helper model for listing valid education levels. """
+
+
+class MaritalStatus(Category):
+    """ Helper model for listing valid education levels. """
+
+
+class ParentOccupation(models.Model):
+    """ Helper model for listing the parent occupations of a Person. """
+    person = models.ForeignKey(
+        'Person',
+        on_delete=models.CASCADE,
+        related_name='parent_occupations',
+    )
+    parent = models.ForeignKey(
+        'Parent',
+        on_delete=models.PROTECT,
+    )
+    occupation = models.CharField(max_length=126)
+
+
+class Parent(Category):
+    """ Helper model for listing valid parent types. """
