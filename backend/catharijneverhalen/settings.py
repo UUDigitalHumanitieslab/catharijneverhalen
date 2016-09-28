5921a86f220a605a7158dfd1a05bd5762a9661d4
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-09-28 13:55:30 +0200

    Add the api package to the INSTALLED_APPS

--- backend/catharijneverhalen/settings.py
+++ backend/catharijneverhalen/settings.py
@@ -19,6 +19,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Application definition
 
 INSTALLED_APPS = [
+    'api.apps.ApiConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
