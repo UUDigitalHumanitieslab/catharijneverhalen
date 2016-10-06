9312eb5ae2c492e7dbcd114709b58bdf17125c3c
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-06 17:24:01 +0200

    Add very basic Person views based on the incomplete serializer

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 app_name = 'api'
 urlpatterns = [
+    url(r'^persons/$', views.PersonList.as_view()),
+    url(r'^persons/(?P<pk>[0-9]+)/$', views.PersonDetail.as_view()),
 ]
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,4 +1,16 @@
-from django.http import HttpResponse
 from django.conf import settings
 
+from rest_framework import generics
 
+from api.models import *
+from api.serializers import *
+
+
+class PersonList(generics.ListCreateAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
+
+
+class PersonDetail(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Person.objects.all()
+    serializer_class = PersonSerializer
