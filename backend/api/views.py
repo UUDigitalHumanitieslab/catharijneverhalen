7cab3e4adaf31ebefa62ac5713fbb46d47f496b5
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-11 17:22:47 +0200

    Apply permissions to the existing API views

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,24 +2,29 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 from rest_framework import generics
+from rest_framework import permissions
 
 from api.models import *
 from api.serializers import *
+from api.permissions import *
 
 
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (permissions.IsAdminUser,)
 
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = (Or(IsOwner, permissions.IsAdminUser),)
 
 
 class PersonList(generics.ListCreateAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    permission_classes = (permissions.IsAdminUser,)
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -28,3 +33,4 @@ class PersonList(generics.ListCreateAPIView):
 class PersonDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
+    permission_classes = (Or(IsOwner, permissions.IsAdminUser),)
