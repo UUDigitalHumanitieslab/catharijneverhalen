c8a3f2dace442c7e18ff867a02a12b9868d71515
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-12 16:37:05 +0200

    Add the missing DRF urlpatterns.format_suffix_patterns

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,11 +1,13 @@
 from django.conf.urls import url
 
+from rest_framework.urlpatterns import format_suffix_patterns
+
 from . import views
 
 app_name = 'api'
-urlpatterns = [
+urlpatterns = format_suffix_patterns([
     url(r'^users/$', views.UserList.as_view()),
     url(r'^users/(?P<pk>[0-9]+)/$', views.UserDetail.as_view()),
     url(r'^persons/$', views.PersonList.as_view()),
     url(r'^persons/(?P<pk>[0-9]+)/$', views.PersonDetail.as_view()),
-]
+])
