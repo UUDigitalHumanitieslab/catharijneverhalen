0ea792528e6b45e3da785e6ab7be1ddb68af8ba1
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-12 12:11:12 +0200

    Use the fixed permissions

--- backend/api/views.py
+++ backend/api/views.py
@@ -2,7 +2,6 @@ from django.conf import settings
 from django.contrib.auth.models import User
 
 from rest_framework import generics
-from rest_framework import permissions
 
 from api.models import *
 from api.serializers import *
@@ -12,19 +11,19 @@ from api.permissions import *
 class UserList(generics.ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (IsAdminUser,)
 
 
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    permission_classes = (Or(IsOwner, permissions.IsAdminUser),)
+    permission_classes = (Or(IsOwner, IsAdminUser),)
 
 
 class PersonList(generics.ListCreateAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
-    permission_classes = (permissions.IsAdminUser,)
+    permission_classes = (IsAdminUser,)
     
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
@@ -33,4 +32,4 @@ class PersonList(generics.ListCreateAPIView):
 class PersonDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Person.objects.all()
     serializer_class = PersonSerializer
-    permission_classes = (Or(IsOwner, permissions.IsAdminUser),)
+    permission_classes = (Or(IsOwner, IsAdminUser),)
