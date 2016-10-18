2b9cb1737d78f77caaf76c3d720c9211b682a0c8
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-18 10:26:32 +0200

    Populate the admin

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from api.models import *
+
+admin.site.register(EducationLevel)
+admin.site.register(MaritalStatus)
+admin.site.register(Parent)
+admin.site.register(Person)
+admin.site.register(UrlStoryAttachment)
+admin.site.register(ImageStoryAttachment)
+admin.site.register(Story)
