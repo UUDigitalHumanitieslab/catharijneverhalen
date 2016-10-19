8a2d081bdb167016f72fe1187ce1294fc44a75d1
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-19 21:47:55 +0200

    Make the registration form actually create an account

--- frontend/app/views/aanmelden/aanmelden.js
+++ frontend/app/views/aanmelden/aanmelden.js
@@ -1,9 +1,17 @@
 'use strict';
 
-angular.module('catharijne.aanmelden', ['ngRoute'])
+angular.module('catharijne.aanmelden', ['ngRoute', 'catharijne.user'])
 
 .config(['$routeProvider', function($routeProvider) {
 	$routeProvider.when('/aanmelden', {
-		templateUrl: 'views/aanmelden/aanmelden.html'
+		templateUrl: 'views/aanmelden/aanmelden.html',
+		controller: 'RegisterCtrl',
 	});
+}])
+
+.controller('RegisterCtrl', ['$scope', 'user', function($scope, user) {
+	$scope.credentials = {};
+	$scope.register = function register(credentials) {
+		user.register(credentials);
+	};
 }]);
