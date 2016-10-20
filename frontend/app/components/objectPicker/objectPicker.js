195f529492dd538b949d0359690c3b41d4bec04f
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-20 20:45:57 +0200

    Define semi-resource interface for objects, update objectPicker

--- frontend/app/components/objectPicker/objectPicker.js
+++ frontend/app/components/objectPicker/objectPicker.js
@@ -2,54 +2,55 @@
 
 angular.module(
 	'catharijne.objectPicker',
-	['catharijne.blocklist']
+	['catharijne.blocklist', 'catharijne.object'],
 ).directive('appObjectPicker', function() {
 	return {
 		scope: {
-			objectId: '='
+			objectUrl: '='
 		},
 		templateUrl: 'components/objectPicker/objectPicker.html',
 		controller: 'ObjectPickerCtrl',
 		replace: true
 	};
-}).controller('ObjectPickerCtrl', function($scope, $http, $log) {
+}).controller('ObjectPickerCtrl', function($scope, $http, $log, object) {
 	var self = this;
 	$scope.picking = false;
 	$scope.pick = function() {
 		$scope.picking = true;
 	};
-	$scope.update = function(objectId) {
-		if (objectId) $scope.objectId = objectId;
-		$scope.object = _.find(self.allObjects, {inventoryID: $scope.objectId});
-		if (! $scope.object) $scope.objectId = undefined;
-	};
-	$http.get('demo_objects.json', {
-		responseType: 'json'
-	}).then(function success(response) {
-		self.allObjects = response.data;
-		$scope.objectDescriptions = _.map(self.allObjects, function(obj) {
-			var dateRange = obj.dateRange, creator = obj.creator;
-			if (typeof dateRange === 'string') {
-				dateRange = dateRange.trim();
-			}
-			if (typeof creator === 'string') {
-				creator = creator.trim();
-			}
-			creator = creator || 'Maker onbekend';
-			dateRange = dateRange || 'datum onbekend';
-			return {
-				imageUrl: 'image/' + obj.image,
-				title: obj.title,
-				description: creator + ', ' + dateRange,
-				linkText: 'Kies',
-				click: function() {
-					$scope.update(obj.inventoryID);
-					$scope.picking = false;
-				},
-			};
+	$scope.update = function(objectUrl) {
+		object.get({url: objectUrl}, function updateSuccess(obj) {
+			$scope.object = obj;
+			$scope.objectUrl = objectUrl;
+		}, function updateFail() {
+			$scope.object = $scope.objectUrl = undefined;
 		});
-		$scope.update();
+	};
+	function transformObject(obj) {
+		var dateRange = obj.dateRange, creator = obj.creator;
+		if (typeof dateRange === 'string') {
+			dateRange = dateRange.trim();
+		}
+		if (typeof creator === 'string') {
+			creator = creator.trim();
+		}
+		creator = creator || 'Maker onbekend';
+		dateRange = dateRange || 'datum onbekend';
+		return {
+			imageUrl: 'image/' + obj.image,
+			title: obj.title,
+			description: creator + ', ' + dateRange,
+			linkText: 'Kies',
+			click: function() {
+				$scope.update(obj.url);
+				$scope.picking = false;
+			},
+		};
+	}
+	object.query().$promise.then(function success(list) {
+		$scope.objectDescriptions = _.map(list, transformObject);
 	}, function fail(response) {
 		$log.debug(response);
 	});
+	$scope.update();
 });
---
+++ frontend/app/components/resources/object.js
@@ -0,0 +1,59 @@
+'use strict';
+
+angular.module('catharijne.object', ['catharijne.resource'])
+.service('object', ['$resource', '$http', '$q', '$location', function($resource, $http, $q, $location) {
+	// This service builds a $resource-like interface.
+	var scheme = $location.protocol();
+	var host = $location.host();
+	var port = $location.port();
+	var portPart = (port === 80 || port === 443 ? '' : ':' + port);
+	var base = scheme + '://' + host + portPart + '/app/demo_objects.json';
+	var transforms = $http.defaults.transformResponse;
+	if (! angular.isArray(transforms)) transforms = [transforms];
+	transforms = transforms.concat(function transform(data, headers, status) {
+		if (angular.isArray(data) && 200 <= status && status < 300) {
+			_.forEach(data, function(obj) {
+				obj.url = base + '#' + obj.inventoryID;
+			});
+		}
+		return data;
+	})
+	var resource = $resource(base, {}, {
+		query: {
+			method: 'get',
+			isArray: true,
+			cache: true,
+			transformResponse: transforms,
+		},
+	});
+	var objectList = resource.query();
+	var service = {};
+	service.query = function(params, success, fail) {
+		objectList.$promise.then(success, fail);
+		return objectList;
+	};
+	service.get = function(params, success, fail) {
+		var object = {};
+		var deferred = $q.defer();
+		object.$resolved = false;
+		object.$promise = deferred.promise;
+		objectList.$promise.then(function getSuccess(list) {
+			var match = _.find(list, params);
+			if (match) {
+				_.assign(object, match);
+				object.$resolved = true;
+				deferred.resolve(object);
+			} else {
+				object.$resolved = true;
+				deferred.reject({status: 404});
+			}
+		}, function getFail(xhr) {
+			object.$resolved = true;
+			deferred.reject(xhr);
+			return xhr;
+		});
+		object.$promise.then(success, fail);
+		return object;
+	};
+	return service;
+}]);
