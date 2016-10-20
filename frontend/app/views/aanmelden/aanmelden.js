17b7261b5815848af4b6e3771a58013346aa3505
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-20 11:16:18 +0200

    Validate and style the form and change view on success

--- frontend/app/views/aanmelden/aanmelden.js
+++ frontend/app/views/aanmelden/aanmelden.js
@@ -9,9 +9,20 @@ angular.module('catharijne.aanmelden', ['ngRoute', 'catharijne.user'])
 	});
 }])
 
-.controller('RegisterCtrl', ['$scope', 'user', function($scope, user) {
+.controller('RegisterCtrl', ['$scope', '$location', 'user', function($scope, $location, user) {
+	function registerSuccess() {
+		$location.path('/herinneringofprofiel');
+	}
+	function registerFail(xhrResponse) {
+		if (xhrResponse.data.username) $scope.serverRejectedUsername = true;
+		if (xhrResponse.data.email) $scope.serverRejectedEmail = true;
+		if (xhrResponse.data.password) $scope.serverRejectedPassword = true;
+	}
 	$scope.credentials = {};
 	$scope.register = function register(credentials) {
-		user.register(credentials);
+		$scope.serverRejectedUsername = false;
+		$scope.serverRejectedEmail = false;
+		$scope.serverRejectedPassword = false;
+		user.register(credentials, registerSuccess, registerFail);
 	};
 }]);
