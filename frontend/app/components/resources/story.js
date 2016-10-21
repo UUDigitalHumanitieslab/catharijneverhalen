bb08168ac7ff52c7ee8c3dd4434c7c50fc19e7d5
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-21 13:00:41 +0200

    Add attachment resources
    The imageAttachment resource is highly nontrivial, because Angular's
    $resource does not support sending data with content types other than
    application/json. So we monkey-patch the .save method with a custom
    $http call using the HTML5 FormData API and some promise tricks.

--- frontend/app/components/resources/story.js
+++ frontend/app/components/resources/story.js
@@ -27,4 +27,56 @@ angular.module('catharijne.story', ['catharijne.resource'])
 			},
 		});
 	},
+]).service('urlAttachment', [
+	'$resource',
+	function urlAttachmentService($resource) {
+		return $resource('/api/url-attachments/', {url: '@url'}, {
+			get: {
+				url: ':url',
+				method: 'get',
+			},
+			update: {
+				url: ':url',
+				method: 'put',
+			},
+			'delete': {
+				url: ':url',
+				method: 'delete',
+			},
+		});
+	},
+]).service('imageAttachment', [
+	'$resource', '$http', '$q',
+	function imageAttachmentService($resource, $http, $q) {
+		// This resource is special, as the save method takes a form as data.
+		var base = '/api/image-attachments/';
+		var service = $resource(base, {url: '@url'}, {
+			get: {
+				url: ':url',
+				method: 'get',
+			},
+			update: undefined,
+			'delete': {
+				url: ':url',
+				method: 'delete',
+			},
+		});
+		service.save = function saveImage(params, form, success, fail) {
+			var formData = new FormData(form);
+			var result = new service();
+			result.$resolved = false;
+			result.$promise = $http.post(base, formData, {
+				headers: {'Content-Type': undefined},
+				transformRequest: angular.identity,
+			}).then(function saveSuccess(attachment) {
+				_.assign(result, attachment);
+				return result;
+			});
+			result.$promise.then(success, fail).finally(function saveResolve() {
+				result.$resolved = true;
+			});
+			return result;
+		};
+		return service;
+	},
 ]);
