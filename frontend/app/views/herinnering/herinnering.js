5145abcf33d80ab024070bad23c811fae4f3f698
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-24 14:36:07 +0200

    Improve the formatting of the story as a whole

--- frontend/app/views/herinnering/herinnering.js
+++ frontend/app/views/herinnering/herinnering.js
@@ -12,19 +12,25 @@ angular.module('catharijne.herinnering', [
 }]).controller('MemoryCtrl', [
 	'$scope', 'story', 'object', '$routeParams',
 	function memoryController($scope, story, object, $routeParams) {
+		function isHeader(text) {
+			return text.trim().split(/\s+/, 12).length < 10;
+		}
 		function formatStory(input) {
-			if (!input) return;
+			var sections = [];
+			if (!input) return sections;
 			var parts = input.split('\n\n');
-			return _.map(parts, function format(text) {
-				var result = {
-					'text': text,
-					'format': 'paragraph',
-				};
-				if (text.trim().split(/\s+/, 12).length < 10) {
-					result.format = 'header';
+			var currentSection;
+			_.forEach(parts, function format(part) {
+				if (isHeader(part)) {
+					if (currentSection) sections.push(currentSection);
+					currentSection = {header: part, paragraphs: []};
+				} else {
+					if (!currentSection) currentSection = {paragraphs: []};
+					currentSection.paragraphs.push(part);
 				}
-				return result;
 			});
+			sections.push(currentSection);
+			return sections;
 		}
 		$scope.story = story.get({pk: $routeParams.pk});
 		$scope.story.$promise.then(function getSubject(storyInstance) {
