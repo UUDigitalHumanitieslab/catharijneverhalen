4df5cd62f49d34392e7d17770896f2b93bb6ddb5
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-25 18:08:05 +0200

    WIP: resourceify the profile page

--- frontend/app/views/profiel/profiel.js
+++ frontend/app/views/profiel/profiel.js
@@ -1,27 +1,33 @@
 'use strict';
 
-angular.module('catharijne.profiel', ['ngRoute'])
-
-.config(['$routeProvider', function($routeProvider) {
-	$routeProvider.when('/profiel/:profileId?', {
+angular.module('catharijne.profiel', [
+	'ngRoute',
+	'catharijne.user',
+	'catharijne.person',
+]).config(['$routeProvider', function($routeProvider) {
+	$routeProvider.when('/profiel/:username?', {
 		templateUrl: 'views/profiel/profiel.html',
-		controller: 'ProfileCtrl'
+		controller: 'ProfileCtrl',
 	});
-}])
-
-.controller('ProfileCtrl', ['$routeParams', '$scope', function($routeParams, $scope) {
-	this.profileId = $routeParams.profileId;
+}]).controller('ProfileCtrl', [
+	'$routeParams', '$scope',
+	function profileController($routeParams, $scope) {
+		var username = $routeParams.username || user.identity.username;
+		if (username && username === user.identity.username) {
+			
+		}
 	
-	$scope.storiesPreview = {
-		title: 'Mijn verhalen',
-		imageUrl: 'verhalenvoorbeeld.jpg',
-		href: '/profiel',
-		linkText: 'Bewerk',
-	};
-	$scope.collectionsPreview = {
-		title: 'Mijn collecties',
-		imageUrl: 'collectiesvoorbeeld.jpg',
-		href: '/profiel',
-		linkText: 'Bewerk',
-	};
-}]);
+		$scope.storiesPreview = {
+			title: 'Mijn verhalen',
+			imageUrl: 'verhalenvoorbeeld.jpg',
+			href: '/profiel',
+			linkText: 'Bewerk',
+		};
+		$scope.collectionsPreview = {
+			title: 'Mijn collecties',
+			imageUrl: 'collectiesvoorbeeld.jpg',
+			href: '/profiel',
+			linkText: 'Bewerk',
+		};
+	},
+]);
