b45e03b41272ca7c4c8ce9255614c7cd87a677e7
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-10-27 17:37:03 +0200

    Also add a way to update a person that does change the portrait

--- frontend/app/components/resources/person.js
+++ frontend/app/components/resources/person.js
@@ -3,8 +3,8 @@
 angular.module('catharijne.person', [
 	'catharijne.resource',
 ]).factory('person', [
-	'$resource', 'appendTransform', 'extractPk',
-	function personServiceFactory($resource, appendTransform, extractPk) {
+	'$resource', '$http', 'appendTransform', 'extractPk',
+	function personServiceFactory($resource, $http, appendTransform, extractPk) {
 		function augment(data, headers, status) {
 			if (!status || 200 <= status && status < 300) {
 				if (data.url) data.pk = extractPk(data.url);
@@ -32,7 +32,8 @@ angular.module('catharijne.person', [
 		function stripPortraitField(data, headers) {
 			return _.omit(data, 'portrait');
 		}
-		return $resource('/api/persons/' + ':pk/', {pk: '@pk'}, {
+		var path = '/api/persons/';
+		var person = $resource(path + ':pk/', {pk: '@pk'}, {
 			query: {
 				method: 'get',
 				isArray: true,
@@ -51,5 +52,36 @@ angular.module('catharijne.person', [
 				transformResponse: appendTransform.response(augment),
 			},
 		});
+		function updatePerson(params, form, success, fail) {
+			// Bind to a person instance before use.
+			var instance = this;
+			var pk = params.pk || instance.pk;
+			var formData = new FormData(form);
+			var birth_date = formData.get('birth_date');
+			if (birth_date && birth_date.length === 4) {
+				formData.set('birth_year', birth_date);
+				formData.set('birth_date', null);
+			}
+			instance.$resolved = false;
+			instance.$promise = $http.put(path + pk + '/', formData, {
+				headers: {'Content-Type': undefined},
+				transformResponse: appendTransform.response(augment),
+			}).then(function updateSuccess(data) {
+				_.assign(instance, data);
+				return instance;
+			});
+			instance.$promise.then(success, fail).finally(function resolve() {
+				instance.$resolved = true;
+			});
+			return instance.$promise;
+		}
+		// Call instance.$update with the form instead of the parameters.
+		person.prototype.$update = _.partial(updatePerson, {});
+		person.update = function personUpdate(params, form, success, fail) {
+			instance = new person();
+			_.bind(updatePerson, instance)(params, form, success, fail);
+			return instance;
+		};
+		return person;
 	},
 ]);
