776eae1575615eb26253d85f163822187ed978a8
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-11-15 17:59:20 +0100

    Use large random image banner for the action choice view

--- frontend/app/views/herinneringofprofiel/herinneringofprofiel.js
+++ frontend/app/views/herinneringofprofiel/herinneringofprofiel.js
@@ -3,12 +3,22 @@
 angular.module('catharijne.herinneringofprofiel', [
 	'ngRoute',
 	'catharijne.authRedirect',
+	'catharijne.object',
 ]).config([
 	'$routeProvider', 'authGuard',
 	function($routeProvider, authGuard) {
 		$routeProvider.when('/herinneringofprofiel', {
 			templateUrl: 'views/herinneringofprofiel/herinneringofprofiel.html',
 			resolve: {redirect: authGuard},
+			controller: 'ActionChoiceCtrl',
 		});
+	},
+]).controller('ActionChoiceCtrl', [
+	'$scope', 'object',
+	function actionChoiceController($scope, object) {
+		function setBannerImage(objectList) {
+			$scope.bannerImage = 'image/' + _.sample(objectList).image;
+		}
+		object.query().$promise.then(setBannerImage);
 	},
 ]);
