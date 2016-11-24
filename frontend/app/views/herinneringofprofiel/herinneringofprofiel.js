d52e39e00e572e8b367e4d281886c73bb8fdbe5b
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-11-24 15:08:54 +0100

    Factor out random banner image logic to banner module

--- frontend/app/views/herinneringofprofiel/herinneringofprofiel.js
+++ frontend/app/views/herinneringofprofiel/herinneringofprofiel.js
@@ -3,22 +3,14 @@
 angular.module('catharijne.herinneringofprofiel', [
 	'ngRoute',
 	'catharijne.authRedirect',
-	'catharijne.object',
+	'catharijne.banner',
 ]).config([
 	'$routeProvider', 'authGuard',
 	function($routeProvider, authGuard) {
 		$routeProvider.when('/herinneringofprofiel', {
 			templateUrl: 'views/herinneringofprofiel/herinneringofprofiel.html',
 			resolve: {redirect: authGuard},
-			controller: 'ActionChoiceCtrl',
+			controller: 'RandomObjectImageCtrl',
 		});
-	},
-]).controller('ActionChoiceCtrl', [
-	'$scope', 'object',
-	function actionChoiceController($scope, object) {
-		function setBannerImage(objectList) {
-			$scope.bannerImage = 'image/' + _.sample(objectList).image;
-		}
-		object.query().$promise.then(setBannerImage);
 	},
 ]);
