9518b6e5fa406a71b099a0fc622ef640276d2476
Julian Gonggrijp <j.gonggrijp@uu.nl>
2016-11-30 11:15:47 +0100

    Karma tests depend on ngResource

--- frontend/karma.conf.js
+++ frontend/karma.conf.js
@@ -8,6 +8,7 @@ module.exports = function(config){
 			'bower_components/angular/angular.js',
 			'bower_components/angular-route/angular-route.js',
 			'bower_components/angular-mocks/angular-mocks.js',
+			'bower_components/angular-resource/angular-resource.js',
 			'bower_components/angular-simple-logger/dist/angular-simple-logger.js',
 			'bower_components/angular-google-maps/dist/angular-google-maps.js',
 			'../unittest-fixtures.js',
